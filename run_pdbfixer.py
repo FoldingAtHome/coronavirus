63b14558f58194668f17162a29a7bd7b6750461c
Rafal P. Wiewiora <rafwiewiora@gmail.com>
2020-01-29 22:13:34 -0500

    high res on low res align rbd-ab fragment sims

---
+++ run_pdbfixer.py
@@ -0,0 +1,14 @@
+import pdbfixer
+from simtk.openmm.app import PDBFile
+
+fixer = pdbfixer.PDBFixer('6nb8_2ajf_complex.pdb')
+fixer.findMissingResidues()
+fixer.findMissingAtoms()
+fixer.addMissingAtoms()
+PDBFile.writeFile(fixer.topology, fixer.positions, open('6nb8_2ajf_complex_fixed.pdb', 'w'))
+
+fixer = pdbfixer.PDBFixer('6nb8_2ghv_complex.pdb')
+fixer.findMissingResidues()
+fixer.findMissingAtoms()
+fixer.addMissingAtoms()
+PDBFile.writeFile(fixer.topology, fixer.positions, open('6nb8_2ghv_complex_fixed.pdb', 'w'))

44a89984302d08eebee970786ca1a9f5dba29417
Hervé Beraud <hberaud@redhat.com>
2020-04-10 00:24:26 +0200

    Refactor simulation script
    Refactor code to reduce technical debt and factorize some redundant
    code.

--- system-preparation/swiss_models/4fs_simulate.py
+++ system-preparation/swiss_models/4fs_simulate.py
@@ -35,25 +35,33 @@ state_xml_filename = 'state_4fs.xml'
 system_load_xml_filename = 'system.xml'
 state_load_xml_filename = 'state.xml'
 
+
+def serialize(filename, data):
+    with open(os.path.join(output_prefix, filename), 'w') as outfile:
+        xml = openmm.XmlSerializer.serialize(data)
+        outfile.write(xml)
+
+
+def deserialize(filename):
+    with open(os.path.join(output_prefix, filename), 'r') as infile:
+        return openmm.XmlSerializer.deserialize(infile.read())
+
+
 # Read in the model
 pdb_filename = output_prefix + 'equilibrated.pdb'
 print('Loading %s' % pdb_filename)
 pdb = app.PDBFile(pdb_filename)
 
 print('Loading OpenMM System...')
-with open(output_prefix + system_load_xml_filename, 'r') as infile:
-    system = openmm.XmlSerializer.deserialize(infile.read())
+system = deserialize(system_load_xml_filename)
 
 # Serialize integrator
 print('Serializing integrator %s' % output_prefix + integrator_xml_filename)
 integrator = LangevinIntegrator(temperature, collision_rate, timestep, splitting)
-with open(output_prefix + integrator_xml_filename, 'w') as outfile:
-    xml = openmm.XmlSerializer.serialize(integrator)
-    outfile.write(xml)
+serialize(integrator_xml_filename, integrator)
 
 print('Loading OpenMM State...')
-with open(output_prefix + state_load_xml_filename, 'r') as infile:
-    state = openmm.XmlSerializer.deserialize(infile.read())
+state = deserialize(state_load_xml_filename)
 context = openmm.Context(system, integrator)
 context.setState(state)
 
@@ -64,21 +72,28 @@ for iteration in progressbar.progressbar(range(niterations)):
     integrator.step(nsteps)
 elapsed_time = (time.time() - initial_time) * unit.seconds
 simulation_time = niterations * nsteps * timestep
-print('    Equilibration took %.3f s for %.3f ns (%8.3f ns/day)' % (elapsed_time / unit.seconds, simulation_time / unit.nanoseconds, simulation_time / elapsed_time * unit.day / unit.nanoseconds))
+print('    Equilibration took %.3f s for %.3f ns (%8.3f ns/day)' % (
+    elapsed_time / unit.seconds,
+    simulation_time / unit.nanoseconds,
+    simulation_time / elapsed_time * unit.day / unit.nanoseconds)
+)
+
 with open(output_prefix + equilibrated_pdb_filename, 'w') as outfile:
-    app.PDBFile.writeFile(pdb.topology, context.getState(getPositions=True,enforcePeriodicBox=True).getPositions(), file=outfile, keepIds=True)
-print('  final   : %8.3f kcal/mol' % (context.getState(getEnergy=True).getPotentialEnergy()/unit.kilocalories_per_mole))
+    app.PDBFile.writeFile(
+        pdb.topology,
+        context.getState(getPositions=True, enforcePeriodicBox=True).getPositions(),
+        file=outfile,
+        keepIds=True)
+
+print('  final   : %8.3f kcal/mol' % (
+    context.getState(getEnergy=True).getPotentialEnergy()/unit.kilocalories_per_mole))
 
 # Serialize state
 print('Serializing state to %s' % output_prefix + state_xml_filename)
 state = context.getState(getPositions=True, getVelocities=True, getEnergy=True, getForces=True)
-with open(output_prefix + state_xml_filename, 'w') as outfile:
-    xml = openmm.XmlSerializer.serialize(state)
-    outfile.write(xml)
+serialize(state_xml_filename, state)
 
 # Serialize system
 print('Serializing System to %s' % output_prefix + system_xml_filename)
 system.setDefaultPeriodicBoxVectors(*state.getPeriodicBoxVectors())
-with open(output_prefix + system_xml_filename, 'w') as outfile:
-    xml = openmm.XmlSerializer.serialize(system)
-    outfile.write(xml)
+serialize(system_xml_filename, system)
